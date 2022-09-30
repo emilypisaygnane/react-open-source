1456775f60fb4ae83cd494e6191ee3bb66105c79
Emily Pisaygnane <episaygnane@me.com>
2022-09-30 14:46:43 -0700

    added css. colored nav bar, moved to the bottom of the page, titles to each page is a large font

--- src/App.js
+++ src/App.js
@@ -2,25 +2,27 @@ import { Link, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { DataTable } from './components/Table/DataTable';
 import { DataChart } from './components/Chart/DataChart';
+import 'bulma/css/bulma.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      <nav>
+      <nav className="navbar is-black is-fixed-bottom">
         <Link to="/">Home</Link>
         <Link to="/table">Player Table</Link>
         <Link to="/charts">Player Charts</Link>
       </nav>
       <Switch>
         <Route exact path="/">
-          <h1>Annual East/West Bowl</h1>
+          <h1 className="title is-1">Annual East/West Bowl</h1>
         </Route>
         <Route path="/table">
-          <h1>Player Table</h1>
-          <DataTable />
+          <h1 className="title is-1">Player Table</h1>
+          <DataTable className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"/>
         </Route>
         <Route path="/charts">
-          <h1>Player Chart</h1>
+          <h1 className="title is-1">Player Chart</h1>
           <DataChart />
         </Route>
       </Switch>
